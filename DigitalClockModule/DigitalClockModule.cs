c570983003f0a0c4aacad15fbebb9566ebb844c2
DJDavid98 <djdavid98@protonmail.com>
2023-06-06 05:28:56 +0200

    Initial public release

--- DigitalClockModule/DigitalClockModule.cs
+++ DigitalClockModule/DigitalClockModule.cs
@@ -4,38 +4,67 @@ namespace DigitalClockModule
 {
     public partial class DigitalClockModule : Module
     {
-        public override string Title => "DigitalClock";
-        public override string Description => "DigitalClock description";
-        public override string Author => "Author";
+        public override string Title => "Digital Clock";
+        public override string Description => "Maps your local time to 60-based floats for use in a digital clock";
+        public override string Author => "DJDavid98";
+        public override string Prefab => "VRCOSC-DigitalClock";
         public override ModuleType Type => ModuleType.General;
-        protected override TimeSpan DeltaUpdate => TimeSpan.MaxValue;
+        protected override TimeSpan DeltaUpdate => TimeSpan.FromSeconds(1);
 
         protected override void CreateAttributes()
         {
-            CreateSetting(DigitalClockSetting.ExampleSetting, "Example Setting", "An example setting", string.Empty);
-            CreateParameter<bool>(DigitalClockParameter.ExampleParameter, ParameterMode.ReadWrite, "ExampleParameterName", "Example Parameter Display Name", "This is an example parameter");
+            CreateSetting(DigitalClockSetting.Mode, "Mode", "Whether the clock should be in 12-hour or 24-hour mode", DigitalClockMode.Twelve);
+
+            CreateParameter<float>(DigitalClockParameter.Hours, ParameterMode.Write, "VRCOSC/DigitalClock/Hours", "Hours", "The current hour 0-59 mapped to a float");
+            CreateParameter<float>(DigitalClockParameter.Minutes, ParameterMode.Write, "VRCOSC/DigitalClock/Minutes", "Minutes", "The current minute 0-59 mapped to a float");
+            CreateParameter<float>(DigitalClockParameter.Seconds, ParameterMode.Write, "VRCOSC/DigitalClock/Seconds", "Seconds", "The current second 0-59 mapped to a float");
         }
 
         protected override void OnModuleStart()
         {
+            base.OnModuleStart();
         }
 
         protected override void OnModuleUpdate()
         {
+            var time = DateTime.Now;
+
+            var hours = time.Hour;
+            var minutes = time.Minute;
+            var seconds = time.Second;
+            var isTwelveHour = GetSetting<DigitalClockMode>(DigitalClockSetting.Mode) == DigitalClockMode.Twelve;
+
+            float hourNormalised = (isTwelveHour ? hours % 12f : hours) / 59f;
+            var minuteNormalised = minutes / 59f;
+            var secondNormalised = seconds / 59f;
+
+
+            SendParameter(DigitalClockParameter.Hours, hourNormalised);
+            SendParameter(DigitalClockParameter.Minutes, minuteNormalised);
+            SendParameter(DigitalClockParameter.Seconds, secondNormalised);
         }
 
         protected override void OnModuleStop()
         {
-        }
-
-        private enum DigitalClockSetting
-        {
-            ExampleSetting
+            base.OnModuleStop();
         }
 
         private enum DigitalClockParameter
         {
-            ExampleParameter
+            Hours,
+            Minutes,
+            Seconds
+        }
+
+        private enum DigitalClockSetting
+        {
+            Mode
+        }
+
+        private enum DigitalClockMode
+        {
+            Twelve,
+            TwentyFour
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# DigitalClock
+# VRCOSC Digital Clock
+
+A digital clock module for [VRCOSC] based on the original [ClockModule].
+It provides 3 `float` parameters (for hours, minutes and seconds) mapped to the range 0-59 for animating the digits of a digital clock.
+
+A prefab is provided with a basic digital clock
+
+[VRCOSC]: https://github.com/VolcanicArts/VRCOSC
+[ClockModule]: https://github.com/VolcanicArts/VRCOSC/blob/2023.601.0/VRCOSC.Modules/Clock/ClockModule.cs
